44e34d4de50a646d6eb236a4cb8c358692a51e5a
Ivan123123 <ivanivanbo@mail.ru>
2016-12-05 12:38:06 +0300

    v3

--- Unit1.cpp
+++ Unit1.cpp
@@ -8,6 +8,22 @@ using namespace std;
 using namespace std;
 int main()
 {
+    char str[80];
+    int sum = 0;
+    bool kur = false;
+    cout << "  Enter string  ";
+    cin.getline(str, 80);
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (isalpha(str[i]) && !(kur))
+        {
+            sum++;
+            kur = true;
+        }
+        if (!isalpha(str[i]))
+            kur = false;
+    }
+    cout << sum << "\n";
     getch();
     return 0;
  
